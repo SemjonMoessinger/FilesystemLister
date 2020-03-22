e7a355cdc43f65a3ae5e4d6d770a3e745b143e5c
Semjon <s.moessinger@gmx.de>
2020-03-22 09:19:22 +0100

    First filesystem operations

---
+++ source/main.cpp
@@ -0,0 +1,23 @@
+﻿#include "main.h"
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+int main()
+{
+	std::cout << "Hello World." << std::endl;
+
+	try {
+        auto currentPath = fs::current_path();
+        std::cout << currentPath << std::endl;
+
+		//  C:/Daten/601 git/FilesystemLister
+        std::cout << fs::file_size("../../../../TestFilesystem/TestFile.txt")
+                  << std::endl;
+	}
+	catch (fs::filesystem_error& e) {
+		std::cout << e.what() << '\n';
+	}
+
+	return 0;
+}
